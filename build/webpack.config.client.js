d1c3efdb7fc085e3c25daca5917baa69c2c9a68b
lzt <571099325@qq.com>
2018-02-22 17:08:46 +0800

    2-7 hot-module-replaceMent

--- build/webpack.config.client.js
+++ build/webpack.config.client.js
@@ -1,4 +1,5 @@
-const path = require('path')
+const path = require('path');
+const webpack = require('webpack');
 const HTMLPlugin = require('html-webpack-plugin');
 
 const isDev = process.env.NODE_ENV === 'development'
@@ -10,7 +11,7 @@ const config = {
     output: {
         filename: '[name].[hash].js',
         path: path.join(__dirname, '../dist'),
-        publicPath: '/public'
+        publicPath: '/public/'
     },
     module: {
         rules : [
@@ -36,11 +37,17 @@ const config = {
     ]
 }
 if (isDev) {
+    config.entry = {
+        app: [
+            'react-hot-loader/patch',
+            path.join(__dirname, '../client/app.js')
+        ]
+    }
     config.devServer = {
         host: '0.0.0.0',
         port: '8888',
         contentBase: path.join(__dirname, '../dist'),
-        // hot: true,
+        hot: true,
         overlay: {
             errors: true
         },
@@ -49,6 +56,7 @@ if (isDev) {
             index: '/public/index.html'
         }
     }
+    config.plugins.push(new webpack.HotModuleReplacementPlugin() )
 }
 module.exports = config;
 // /public/app.hash.js
--- client/App.jsx
+++ client/App.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default class App extends React.Component {
     render() {
         return (
-            <p>This is app</p>
+            <p>This is app 1233</p>
         )
     }
 }
