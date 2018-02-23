4dcbaef7c957501ffaaeccc3f2fd2cca535c8d47
lzt <571099325@qq.com>
2018-02-23 11:06:58 +0800

    2-8开发时的服务端渲染

--- client/App.jsx
+++ client/App.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default class App extends React.Component {
     render() {
         return (
-            <p>This is app 1233</p>
+            <p>This is app 123</p>
         )
     }
 }
--- server/server.js
+++ server/server.js
@@ -1,20 +1,28 @@
 const express = require('express');
 const ReactSSR = require('react-dom/server');
-const serverEntry = require('../dist/server-entry').default;
 const fs = require('fs');
 const path = require('path');
 
-const template = fs.readFileSync(path.join(__dirname, '../dist/index.html'), 'utf8')
+const isDev = process.env.NODE_ENV === 'development';
+
 
 const app = express();
 
-app.use('/public', express.static(path.join(__dirname, '../dist')))
 
-app.get('*', function (req, res) {
-    const appString = ReactSSR.renderToString(serverEntry);
 
-    res.send(template.replace('<!-- app -->', appString));
-})
+if(!isDev) {
+    const serverEntry = require('../dist/server-entry.js').default;
+    const template = fs.readFileSync(path.join(__dirname, '../dist/index.html'), 'utf8');
+    app.use('/public', express.static(path.join(__dirname, '../dist')));
+    app.get('*', function (req, res) {
+        const appString = ReactSSR.renderToString(serverEntry);
+    
+        res.send(template.replace('<!-- app -->', appString));
+    });
+}else {
+    const devStatic = require('./util/dev-static.js');
+    devStatic(app);
+}
 
 app.listen(3333, function () {
     console.log('server is listening on 3333')
