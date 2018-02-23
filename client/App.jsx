89f74fc8ef0c22395fd4fb0ffe2174022d2a4739
lzt <571099325@qq.com>
2018-02-23 17:37:26 +0800

    2-9 eslint and editorconfig

--- client/App.jsx
+++ client/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 
-export default class App extends React.Component {
-    render() {
-        return (
-            <p>This is app 123</p>
-        )
-    }
-}
+export default () => (<p>This is app 123</p>)
+
+// export default class App extends React.Component {
+//     render() {
+//         return (
+//           <p>This is app 123</p>
+//         )
+//     }
+// }
