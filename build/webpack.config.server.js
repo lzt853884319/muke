c30a548ced7b381ae8a0d4972aab65c163255c50
lzt <571099325@qq.com>
2018-02-24 18:54:06 +0800

    2-10工程架构优化

--- build/webpack.config.server.js
+++ build/webpack.config.server.js
@@ -1,40 +1,15 @@
 const path = require('path')
-module.exports = {
-    target: 'node',
-    entry: {
-        app: path.join(__dirname, '../client/server-entry.js')
-    },
-    output: {
-        filename: 'server-entry.js',
-        path: path.join(__dirname, '../dist'),
-        publicPath: '/public',
-        libraryTarget: 'commonjs2'
-    },
-    module: {
-        rules : [
-            {
-                enforce: 'pre',
-                test: /.(js|jsx)$/,
-                loader: 'eslint-loader',
-                exclude: [
-                    path.join(__dirname, "../node_modules")
-                ]
-            },
-            {
-                test: /.jsx$/,
-                loader: 'babel-loader',
-
-            },
-            {
-                test: /.js$/,
-                loader: 'babel-loader',
-                exclude: [
-                    path.join(__dirname, '../node_modules')
-                ]
-
-            }
-        ]
-    }
-}
+const webpackMerge = require('webpack-merge')
+const baseConfig = require('./webpack.base')
+module.exports = webpackMerge(baseConfig, {
+  target: 'node',
+  entry: {
+    app: path.join(__dirname, '../client/server-entry.js')
+  },
+  output: {
+    filename: 'server-entry.js',
+    libraryTarget: 'commonjs2'
+  }
+})
 
 // /public/app.hash.js
